2b1a9f8836fd5e8745557afe0e0dd1951fc639a1
Hxxdhiman <158607541+Hxxdhiman@users.noreply.github.com>
2024-02-07 07:10:39 +0000

    Update LoginApplication.java

--- blogsApp/login/src/main/java/com/blogs/login/LoginApplication.java
+++ blogsApp/login/src/main/java/com/blogs/login/LoginApplication.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.filter.TokenFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,12 +14,5 @@ public class LoginApplication {
 		SpringApplication.run(LoginApplication.class, args);
 		System.out.println("Running Login/Register Microservice");
 	}
-//	@Bean
-//	public FilterRegistrationBean getFilter(){
-//		FilterRegistrationBean filterReg= new FilterRegistrationBean();
-//		filterReg.setFilter(new filter());
-////		filterReg.addUrlPatterns("/auth/getAll");
-//		return filterReg;
-//	}
 
 }
