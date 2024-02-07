51012a869f6b557c980570877c9575fa481ac28f
Hxxdhiman <158607541+Hxxdhiman@users.noreply.github.com>
2024-02-07 07:11:58 +0000

    Update DashboardApplication.java

--- blogsApp/dashboard/src/main/java/com/blogs/dashboard/DashboardApplication.java
+++ blogsApp/dashboard/src/main/java/com/blogs/dashboard/DashboardApplication.java
@@ -3,7 +3,6 @@ import com.blogs.dashboard.filter.filter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
